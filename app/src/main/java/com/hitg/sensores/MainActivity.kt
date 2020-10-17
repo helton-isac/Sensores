0fdc77cd501f9999bdbc2e8361eed935fc36ee1b
Helton Isac <helton.isac@gmail.com>
2020-10-17 13:55:19 -0300

    Logging Sensors

--- app/src/main/java/com/hitg/sensores/MainActivity.kt
+++ app/src/main/java/com/hitg/sensores/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.hitg.sensores
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        printSensors()
+    }
+
+    private fun printSensors() {
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as
+                SensorManager
+        val sensorList: List<Sensor> = sensorManager.getSensorList(Sensor.TYPE_ALL)
+        for (sensor in sensorList) {
+            Log.i("SENSOR", "Nome: ${sensor.name} - Tipo ${sensor.type} - ${sensor.stringType} ")
+        }
     }
 }
